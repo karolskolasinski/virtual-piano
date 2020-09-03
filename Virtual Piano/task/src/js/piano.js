25f4bf3c8848995ba6bee2b2527298707b2624d7
Karol Skolasiński <karolskolasinski@gmail.com>
2020-09-03 17:29:22 +0200

    Stage 5/5: Adding and styling new keys

--- Virtual Piano/task/src/js/piano.js
+++ Virtual Piano/task/src/js/piano.js
@@ -1,6 +1,6 @@
-const pianoKeys = new Set('ASDFGHJ');
+const pianoKeys = new Set('ASDFGHJWERTYU');
 
-document.addEventListener("keypress", function (e) {
+document.addEventListener("keydown", function (e) {
     if (pianoKeys.has(e.key.toUpperCase())) {
         new Audio("audio/" + e.key.toUpperCase() + ".mp3").play();
     }
