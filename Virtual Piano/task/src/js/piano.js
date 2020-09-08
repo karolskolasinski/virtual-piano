142ce3622f47c7c7e272c8f3299e3876d0c47b2e
Karol Skolasiński <karolskolasinski@gmail.com>
2020-09-08 20:28:12 +0200

    flexbox / style white-keys

--- Virtual Piano/task/src/js/piano.js
+++ Virtual Piano/task/src/js/piano.js
@@ -1,7 +1,22 @@
-const pianoKeys = new Set('ASDFGHJWERTYU');
+// const pianoKeys = new Set('ASDFGHJWERTYU');
+//
+// document.addEventListener("keydown", function (e) {
+//     if (pianoKeys.has(e.key.toUpperCase())) {
+//         new Audio("audio/" + e.key.toUpperCase() + ".mp3").play();
+//     }
+// })
 
-document.addEventListener("keydown", function (e) {
-    if (pianoKeys.has(e.key.toUpperCase())) {
-        new Audio("audio/" + e.key.toUpperCase() + ".mp3").play();
+const keys = {
+    "KeyA": "audio/A.mp3", 'KeyS': "audio/S.mp3",
+    'KeyD': "audio/D.mp3", 'KeyF': "audio/F.mp3",
+    'KeyG': "audio/G.mp3", 'KeyH': "audio/H.mp3",
+    'KeyJ': "audio/J.mp3", 'KeyW': "audio/W.mp3",
+    'KeyE': "audio/E.mp3", 'KeyT': "audio/T.mp3",
+    'KeyY': "audio/Y.mp3", 'KeyU': "audio/U.mp3",
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.code in keys) {
+        new Audio(keys[e.code]).play()
     }
-})
+});
