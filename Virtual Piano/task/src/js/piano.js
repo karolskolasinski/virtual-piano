95300a0023327547349ca7d60d19c2e55f6b3b20
Karol Skolasiński <karolskolasinski@gmail.com>
2020-09-13 19:37:01 +0200

    click

--- Virtual Piano/task/src/js/piano.js
+++ Virtual Piano/task/src/js/piano.js
@@ -2,48 +2,56 @@ document.onkeydown = t => {
     if (t.code === 'Tab') {
         return false;
     }
-}
+};
 
 const keys = {
     //normal keys
-    "Tab": "audio/C1.mp3",
-    "KeyQ": "audio/D1.mp3",
-    'KeyW': "audio/E1.mp3",
-    'KeyE': "audio/F1.mp3",
-    'KeyR': "audio/G1.mp3",
-    'KeyT': "audio/A1.mp3",
-    'KeyY': "audio/B1.mp3",
-    'KeyU': "audio/C2.mp3",
-    'KeyI': "audio/D2.mp3",
-    'KeyO': "audio/E2.mp3",
-    'KeyP': "audio/F2.mp3",
-    'BracketLeft': "audio/G2.mp3",
-    'BracketRight': "audio/A2.mp3",
-    'Backslash': "audio/B2.mp3",
+    'Tab': 'audio/C1.mp3',
+    'KeyQ': 'audio/D1.mp3',
+    'KeyW': 'audio/E1.mp3',
+    'KeyE': 'audio/F1.mp3',
+    'KeyR': 'audio/G1.mp3',
+    'KeyT': 'audio/A1.mp3',
+    'KeyY': 'audio/B1.mp3',
+    'KeyU': 'audio/C2.mp3',
+    'KeyI': 'audio/D2.mp3',
+    'KeyO': 'audio/E2.mp3',
+    'KeyP': 'audio/F2.mp3',
+    'BracketLeft': 'audio/G2.mp3',
+    'BracketRight': 'audio/A2.mp3',
+    'Backslash': 'audio/B2.mp3',
 
     // sharp keys
-    'Digit1': "audio/C1-sharp.mp3",
-    'Digit2': "audio/D1-sharp.mp3",
-    'Digit4': "audio/F1-sharp.mp3",
-    'Digit5': "audio/G1-sharp.mp3",
-    'Digit6': "audio/A1-sharp.mp3",
-    'Digit8': "audio/C2-sharp.mp3",
-    'Digit9': "audio/D2-sharp.mp3",
-    'Minus': "audio/F2-sharp.mp3",
-    'Equal': "audio/G2-sharp.mp3",
-    'Backspace': "audio/A2-sharp.mp3",
-}
+    'Digit1': 'audio/C1-sharp.mp3',
+    'Digit2': 'audio/D1-sharp.mp3',
+    'Digit4': 'audio/F1-sharp.mp3',
+    'Digit5': 'audio/G1-sharp.mp3',
+    'Digit6': 'audio/A1-sharp.mp3',
+    'Digit8': 'audio/C2-sharp.mp3',
+    'Digit9': 'audio/D2-sharp.mp3',
+    'Minus': 'audio/F2-sharp.mp3',
+    'Equal': 'audio/G2-sharp.mp3',
+    'Backspace': 'audio/A2-sharp.mp3',
+};
 
 
-document.addEventListener("keydown", (e) => {
+document.addEventListener('keydown', e => {
     if (e.code in keys) {
         new Audio(keys[e.code]).play();
         document.getElementById(e.code).classList.add('active');
     }
 });
 
-document.addEventListener("keyup", (e) => {
+
+document.addEventListener('keyup', e => {
     if (e.code in keys) {
         document.getElementById(e.code).classList.remove('active');
     }
 });
+
+
+for (let key in keys) {
+    document.getElementById(key).addEventListener("click", () => {
+        new Audio(keys[key]).play();
+    });
+}
